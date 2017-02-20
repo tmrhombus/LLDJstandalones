39474f1520dd48301fc8ac9c17fbd4b48f656b4f
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-02-20 09:35:50 -0600

    regex correction

--- commontools/makelists.sh
+++ commontools/makelists.sh
@@ -41,7 +41,7 @@ then
  printf "Making DY50\n" 
  printf "crosssection: %s\n" ${xcDY50} > ${outdir}/DY50.txt
  grep -i 'DYJetsToLL_M-50' ${outdir}/allfiles.txt  >> ${outdir}/DY50.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/DY50.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/DY50.txt
 fi
 
 if [ ${doDY5to50} = true ]
@@ -49,7 +49,7 @@ then
  printf "Making DY5to50\n" 
  printf "crosssection: %s\n" ${xcDY5to50} > ${outdir}/DY5to50.txt
  grep -i 'DYJetsToLL_M-5to50' ${outdir}/allfiles.txt  >> ${outdir}/DY5to50.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/DY5to50.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/DY5to50.txt
 fi
 
 if [ ${doTTbar} = true ]
@@ -57,7 +57,7 @@ then
  printf "Making TTbar\n" 
  printf "crosssection: %s\n" ${xcTTbar} > ${outdir}/TTbar.txt
  grep -i 'TT_TuneCUETP8M1' ${outdir}/allfiles.txt  >> ${outdir}/TTbar.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/TTbar.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/TTbar.txt
 fi
 
 if [ ${doSTs} = true ]
@@ -65,7 +65,7 @@ then
  printf "Making STs\n" 
  printf "crosssection: %s\n" ${xcSTs} > ${outdir}/STs.txt
  grep -i 'ST_s-channel_4f' ${outdir}/allfiles.txt  >> ${outdir}/STs.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/STs.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STs.txt
 fi
 
 if [ ${doSTtbar} = true ]
@@ -73,7 +73,7 @@ then
  printf "Making STtbar\n" 
  printf "crosssection: %s\n" ${xcSTtbar} > ${outdir}/STtbar.txt
  grep -i 'ST_t-channel_antitop_4f' ${outdir}/allfiles.txt  >> ${outdir}/STtbar.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/STtbar.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtbar.txt
 fi
 
 if [ ${doSTt} = true ]
@@ -81,7 +81,7 @@ then
  printf "Making STt\n" 
  printf "crosssection: %s\n" ${xcSTt} > ${outdir}/STt.txt
  grep -i 'ST_t-channel_top_4f' ${outdir}/allfiles.txt  >> ${outdir}/STt.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/STt.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STt.txt
 fi
 
 if [ ${doSTtbarW} = true ]
@@ -89,7 +89,7 @@ then
  printf "Making STtbarW\n" 
  printf "crosssection: %s\n" ${xcSTtbarW} > ${outdir}/STtbarW.txt
  grep -i 'ST_tW_antitop_5f' ${outdir}/allfiles.txt  >> ${outdir}/STtbarW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/STtbarW.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtbarW.txt
 fi
 
 if [ ${doSTtW} = true ]
@@ -97,7 +97,7 @@ then
  printf "Making STtW\n" 
  printf "crosssection: %s\n" ${xcSTtW} > ${outdir}/STtW.txt
  grep -i 'ST_tW_top_5f' ${outdir}/allfiles.txt  >> ${outdir}/STtW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/STtW.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtW.txt
 fi
 
 if [ ${doWJets} = true ]
@@ -105,7 +105,7 @@ then
  printf "Making WJets\n" 
  printf "crosssection: %s\n" ${xcWJets} > ${outdir}/WJets.txt
  grep -i 'WJetsToLNu' ${outdir}/allfiles.txt  >> ${outdir}/WJets.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/WJets.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WJets.txt
 fi
 
 if [ ${doZHtoLLbb} = true ]
@@ -113,7 +113,7 @@ then
  printf "Making ZHtoLLbb\n" 
  printf "crosssection: %s\n" ${xcZHtoLLbb} > ${outdir}/ZHtoLLbb.txt
  grep -i 'ZH_HToBB_ZToLL_M125' ${outdir}/allfiles.txt  >> ${outdir}/ZHtoLLbb.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/ZHtoLLbb.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/ZHtoLLbb.txt
 fi
 
 if [ ${doWW} = true ]
@@ -121,7 +121,7 @@ then
  printf "Making WW\n" 
  printf "crosssection: %s\n" ${xcWW} > ${outdir}/WW.txt
  grep -i 'WW_TuneCUETP8M1' ${outdir}/allfiles.txt  >> ${outdir}/WW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/WW.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WW.txt
 fi
 
 if [ ${doZZ} = true ]
@@ -129,7 +129,7 @@ then
  printf "Making ZZ\n" 
  printf "crosssection: %s\n" ${xcZZ} > ${outdir}/ZZ.txt
  grep -i 'ZZ_TuneCUETP8M1' ${outdir}/allfiles.txt  >> ${outdir}/ZZ.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/ZZ.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/ZZ.txt
 fi
 
 if [ ${doWZ} = true ]
@@ -137,15 +137,15 @@ then
  printf "Making WZ\n" 
  printf "crosssection: %s\n" ${xcWZ} > ${outdir}/WZ.txt
  grep -i 'WZ_TuneCUETP8M1' ${outdir}/allfiles.txt  >> ${outdir}/WZ.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/WZ.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WZ.txt
 fi
 
 if [ ${doSignal} = true ]
 then
  printf "Making Signal\n" 
  printf "crosssection: %s\n" ${xcSignal} > ${outdir}/Signal.txt
- grep -i 'ggZH_HToSSTobbbb' ${outdir}/allfiles.txt  >> ${outdir}/Signal.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHisto@' ${outdir}/Signal.txt
+ grep -i 'HToSSTobbbb' ${outdir}/allfiles.txt  >> ${outdir}/Signal.txt
+ sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/Signal.txt
 fi
 
 
