e127dc74ced9fdf467e0c556ea52b548dbe873ee
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-03-06 09:20:28 -0600

    adapt to ben's file structure

--- commontools/makelists.sh
+++ commontools/makelists.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 outdir="${CMSSW_BASE}/src/LLDJstandalones/lists"
 mkdir -p "${outdir}"
- 
-eos root://cmseos.fnal.gov ls /store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/ > ${outdir}/allfiles.txt
+
+initpath="/store/group/lpchbb/kreis/AnalysisTrees"
+eos root://cmseos.fnal.gov ls ${initpath} > ${outdir}/allfiles.txt
  
 doDY50=true
 doDY5to50=true
@@ -39,99 +39,134 @@ xcSignal="1"
 if [ ${doDY50} = true ]
 then
  printf "Making DY50\n" 
- grep -i 'DYJetsToLL_M-50' ${outdir}/allfiles.txt  > ${outdir}/DY50.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/DY50.txt
+ fullsamplename=$(grep -i 'DYJetsToLL_M-50_Tune' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/DY50.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/DY50.txt
 fi
 
 if [ ${doDY5to50} = true ]
 then
  printf "Making DY5to50\n" 
- grep -i 'DYJetsToLL_M-5to50' ${outdir}/allfiles.txt  > ${outdir}/DY5to50.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/DY5to50.txt
+ fullsamplename=$(grep -i 'DYJetsToLL_M-5to50' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/DY5to50.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/DY5to50.txt
 fi
 
 if [ ${doTTbar} = true ]
 then
  printf "Making TTbar\n" 
- grep -i 'TT_TuneCUETP8M1' ${outdir}/allfiles.txt  > ${outdir}/TTbar.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/TTbar.txt
+ fullsamplename=$(grep -i 'TT_TuneCUETP8M1' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/TTbar.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/TTbar.txt
 fi
 
 if [ ${doSTs} = true ]
 then
  printf "Making STs\n" 
- grep -i 'ST_s-channel_4f' ${outdir}/allfiles.txt  > ${outdir}/STs.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STs.txt
+ fullsamplename=$(grep -i 'ST_s-channel_4f' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/STs.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/STs.txt
 fi
 
 if [ ${doSTtbar} = true ]
 then
  printf "Making STtbar\n" 
- grep -i 'ST_t-channel_antitop_4f' ${outdir}/allfiles.txt  > ${outdir}/STtbar.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtbar.txt
+ fullsamplename=$(grep -i 'ST_t-channel_antitop_4f' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/STtbar.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/STtbar.txt
 fi
 
 if [ ${doSTt} = true ]
 then
  printf "Making STt\n" 
- grep -i 'ST_t-channel_top_4f' ${outdir}/allfiles.txt  > ${outdir}/STt.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STt.txt
+ fullsamplename=$(grep -i 'ST_t-channel_top_4f' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/STt.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/STt.txt
 fi
 
 if [ ${doSTtbarW} = true ]
 then
  printf "Making STtbarW\n" 
- grep -i 'ST_tW_antitop_5f' ${outdir}/allfiles.txt  > ${outdir}/STtbarW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtbarW.txt
+ fullsamplename=$(grep -i 'ST_tW_antitop_5f' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/STtbarW.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/STtbarW.txt
 fi
 
 if [ ${doSTtW} = true ]
 then
  printf "Making STtW\n" 
- grep -i 'ST_tW_top_5f' ${outdir}/allfiles.txt  > ${outdir}/STtW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/STtW.txt
+ fullsamplename=$(grep -i 'ST_tW_top_5f' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/STtW.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/STtW.txt
 fi
 
 if [ ${doWJets} = true ]
 then
  printf "Making WJets\n" 
- grep -i 'WJetsToLNu' ${outdir}/allfiles.txt  > ${outdir}/WJets.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WJets.txt
+ fullsamplename=$(grep -i 'WJetsToLNu' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/WJets.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/WJets.txt
 fi
 
 if [ ${doZHtoLLbb} = true ]
 then
  printf "Making ZHtoLLbb\n" 
- grep -i 'ZH_HToBB_ZToLL_M125' ${outdir}/allfiles.txt  > ${outdir}/ZHtoLLbb.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/ZHtoLLbb.txt
+ fullsamplename=$(grep -i 'ZH_HToBB_ZToLL' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ for sample in ${fullsamplename}
+ do
+  printf "%s/%s\n" ${initpath} ${sample}
+  eos root://cmseos.fnal.gov ls ${initpath}/${sample} > ${outdir}/${sample}.txt
+  sed -i -e "s@histo_@${initpath}/${sample}/histo_@" ${outdir}/${sample}.txt
+ done
 fi
 
 if [ ${doWW} = true ]
 then
  printf "Making WW\n" 
- grep -i 'WW_TuneCUETP8M1' ${outdir}/allfiles.txt  > ${outdir}/WW.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WW.txt
+ fullsamplename=$(grep -i 'WW_TuneCUETP8M1' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/WW.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/WW.txt
 fi
 
 if [ ${doZZ} = true ]
 then
  printf "Making ZZ\n" 
- grep -i 'ZZ_TuneCUETP8M1' ${outdir}/allfiles.txt  > ${outdir}/ZZ.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/ZZ.txt
+ fullsamplename=$(grep -i 'ZZ_TuneCUETP8M1' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/ZZ.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/ZZ.txt
 fi
 
 if [ ${doWZ} = true ]
 then
  printf "Making WZ\n" 
- grep -i 'WZ_TuneCUETP8M1' ${outdir}/allfiles.txt  > ${outdir}/WZ.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/WZ.txt
+ fullsamplename=$(grep -i 'WZ_TuneCUETP8M1' ${outdir}/allfiles.txt)
+ printf "%s/%s\n" ${initpath} ${fullsamplename}
+ eos root://cmseos.fnal.gov ls ${initpath}/${fullsamplename} > ${outdir}/WZ.txt
+ sed -i -e "s@histo_@${initpath}/${fullsamplename}/histo_@" ${outdir}/WZ.txt
 fi
 
 if [ ${doSignal} = true ]
 then
  printf "Making Signal\n" 
- grep -i 'HToSSTobbbb' ${outdir}/allfiles.txt  > ${outdir}/Signal.txt
- sed -i -e 's@allHistos@/store/group/lpchbb/noreplica/stata/AnalysisTrees/addedHistos/allHistos@' ${outdir}/Signal.txt
+ fullsamplename=$(grep -i 'HToSSTobbbb' ${outdir}/allfiles.txt)
+ for sample in ${fullsamplename}
+ do
+  printf "%s/%s\n" ${initpath} ${sample}
+  eos root://cmseos.fnal.gov ls ${initpath}/${sample} > ${outdir}/Signal_${sample}.txt
+  sed -i -e "s@histo_@${initpath}/${sample}/histo_@" ${outdir}/Signal_${sample}.txt
+ done
 fi
 
 
