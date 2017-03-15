fece06f7a5a72b3da9fb2ae3aad19fc74bb710f6
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-03-14 19:34:45 -0500

    bash functions, split list/info

--- commontools/countevents.cxx
+++ commontools/countevents.cxx
@@ -2,21 +2,18 @@
 #include <stdio.h>
 #include <cmath>
  
-void countevents(TString listname, Bool_t doSingle=kFALSE){
+double countevents(TString Tsample){
  
- printf("listname %s\n",listname.Data());
  Double_t ntotal = 0;
  Double_t nevents = 0;
 
- // doSingle reads out events for every file individually
- printf("doSingle %i\n",doSingle);
+ char* listdir;
+ listdir = getenv ("listdir");
+ TString Tlistdir = TString(listdir);
+ TString fulllistname = Tlistdir+"/"+Tsample+".list";
+ TString fulloutname   = Tlistdir+"/"+Tsample+".info";
 
- char* CMSSW_BASE;
- submitbase = getenv ("CMSSW_BASE");
- TString Tsubmitbase = TString(submitbase);
- TString fulllistname = Tsubmitbase+"/src/LLDJstandalones/lists/"+listname;
-
- printf("listname : %s \n",fulllistname.Data());
+ printf("Tsample : %s \n",fulllistname.Data());
 
  std::fstream inputfile;
  inputfile.open(fulllistname);
@@ -51,38 +48,17 @@ void countevents(TString listname, Bool_t doSingle=kFALSE){
 
    theFile->Close();
 
-   // write each line w/ nrevents to indivdual file
-   if(doSingle){
-
-    // manually get outname from inputname on list.. ug
-    std::string outname = inputline;
-    UInt_t erase_a = outname.find("allHistos");
-    outname.erase(0,erase_a+10);
-    UInt_t erase_b = outname.find("_TuneCUETP");
-    outname.erase(erase_b,erase_b+40);
-    printf(" outname %s\n",outname.c_str());
-    TString Toutname = outname;
-
-    TString fulloutname = Tsubmitbase+"/src/LLDJstandalones/lists/"+outname+".txt";
-
-    FILE * outfile;
-    outfile = fopen (fulloutname,"w");
-    fprintf (outfile, "%s \n",Tinputline.Data());
-    fprintf (outfile, "crosssection: 1.0 \n");
-    fprintf (outfile, "nrevents: %.1f \n",nevents);
-    fclose (outfile);
- 
-   } // if doSingle
   } // if line contains /store/group
  } //while getline
  inputfile.close();
 
  // append nrevents to list file
  FILE * outfile;
- outfile = fopen (fulllistname,"a");
+ outfile = fopen (fulloutname,"w");
  fprintf (outfile, "nrevents: %.1f \n",ntotal);
  fclose (outfile);
 
  printf("--total: %.1f\n",ntotal);
+ return ntotal;
  
 }
--- commontools/countevents.sh
+++ commontools/countevents.sh
@@ -32,129 +32,45 @@ xcZZ="63.0"
 xcWZ="118.7"                  
 xcSignal="1"         
 
-if [ ${doDY50} = true ]
-then
- printf "Making DY50\n" 
- grep -v "crosssection" ${outdir}/DY50.txt > temp && mv temp ${outdir}/DY50.txt 
- grep -v "nrevents" ${outdir}/DY50.txt > temp && mv temp ${outdir}/DY50.txt 
- printf "crosssection: %s\n" ${xcDY50} >> ${outdir}/DY50.txt
- root -l -b -q countevents.cxx\('"DY50.txt"'\)
-fi
+countalist () {
+ if [ $1 = true ]
+ then
+  printf "Making %s\n" $2
+  root -l -b -q 'countevents.cxx("'$2'")'
+  printf "crosssection: %s\n" $3 >> ${listdir}/$2.info
+ fi
+}
 
-if [ ${doDY5to50} = true ]
-then
- printf "Making DY5to50\n" 
- grep -v "crosssection" ${outdir}/DY5to50.txt > temp && mv temp ${outdir}/DY5to50.txt 
- grep -v "nrevents" ${outdir}/DY5to50.txt > temp && mv temp ${outdir}/DY5to50.txt 
- printf "crosssection: %s\n" ${xcDY5to50} >> ${outdir}/DY5to50.txt
- root -l -b -q countevents.cxx\('"DY5to50.txt"'\)
-fi
+countmanylists () {
+ if [ $1 = true ]
+ then
+  printf "Making %s\n" $2
+  for fullsample in $(ls ${listdir}/$2*list)
+  do
 
-if [ ${doTTbar} = true ]
-then
- printf "Making TTbar\n" 
- grep -v "crosssection" ${outdir}/TTbar.txt > temp && mv temp ${outdir}/TTbar.txt 
- grep -v "nrevents" ${outdir}/TTbar.txt > temp && mv temp ${outdir}/TTbar.txt 
- printf "crosssection: %s\n" ${xcTTbar} >> ${outdir}/TTbar.txt
- root -l -b -q countevents.cxx\('"TTbar.txt"'\)
-fi
+   sample=$( printf "%s" ${fullsample} | sed -e 's/.*'$2'/'$2'/' | sed -e 's/\.list//' )
+   #printf "Sample is %s\n" ${sample}
 
-if [ ${doSTs} = true ]
-then
- printf "Making STs\n" 
- grep -v "crosssection" ${outdir}/STs.txt > temp && mv temp ${outdir}/STs.txt 
- grep -v "nrevents" ${outdir}/STs.txt > temp && mv temp ${outdir}/STs.txt 
- printf "crosssection: %s\n" ${xcSTs} >> ${outdir}/STs.txt
- root -l -b -q countevents.cxx\('"STs.txt"'\)
-fi
+   root -l -b -q 'countevents.cxx("'${sample}'")'
+   printf "crosssection: %s\n" $3 >> ${listdir}/${sample}.info
+  done # for sample in Signal*txt
 
-if [ ${doSTtbar} = true ]
-then
- printf "Making STtbar\n" 
- grep -v "crosssection" ${outdir}/STtbar.txt > temp && mv temp ${outdir}/STtbar.txt 
- grep -v "nrevents" ${outdir}/STtbar.txt > temp && mv temp ${outdir}/STtbar.txt 
- printf "crosssection: %s\n" ${xcSTtbar} >> ${outdir}/STtbar.txt
- root -l -b -q countevents.cxx\('"STtbar.txt"'\)
-fi
+ fi
+}
 
-if [ ${doSTt} = true ]
-then
- printf "Making STt\n" 
- grep -v "crosssection" ${outdir}/STt.txt > temp && mv temp ${outdir}/STt.txt 
- grep -v "nrevents" ${outdir}/STt.txt > temp && mv temp ${outdir}/STt.txt 
- printf "crosssection: %s\n" ${xcSTt} >> ${outdir}/STt.txt
- root -l -b -q countevents.cxx\('"STt.txt"'\)
-fi
+countalist ${doDY50}      "DY50"       ${xcDY50}     
+countalist ${doDY5to50}   "DY5to50"    ${xcDY5to50}  
+countalist ${doTTbar}     "TTbar"      ${xcTTbar}    
+countalist ${doSTs}       "STs"        ${xcSTs}      
+countalist ${doSTtbar}    "STtbar"     ${xcSTtbar}   
+countalist ${doSTt}       "STt"        ${xcSTt}      
+countalist ${doSTtbarW}   "STtbarW"    ${xcSTtbarW}  
+countalist ${doSTtW}      "STtW"       ${xcSTtW}     
+countalist ${doWJets}     "WJets"      ${xcWJets}    
+countalist ${doZHtoLLbb}  "ZHtoLLbb"   ${xcZHtoLLbb} 
+countalist ${doWW}        "WW"         ${xcWW}       
+countalist ${doZZ}        "ZZ"         ${xcZZ}       
+countalist ${doWZ}        "WZ"         ${xcWZ}       
 
-if [ ${doSTtbarW} = true ]
-then
- printf "Making STtbarW\n" 
- grep -v "crosssection" ${outdir}/STtbarW.txt > temp && mv temp ${outdir}/STtbarW.txt 
- grep -v "nrevents" ${outdir}/STtbarW.txt > temp && mv temp ${outdir}/STtbarW.txt 
- printf "crosssection: %s\n" ${xcSTtbarW} >> ${outdir}/STtbarW.txt
- root -l -b -q countevents.cxx\('"STtbarW.txt"'\)
-fi
-
-if [ ${doSTtW} = true ]
-then
- printf "Making STtW\n" 
- grep -v "crosssection" ${outdir}/STtW.txt > temp && mv temp ${outdir}/STtW.txt 
- grep -v "nrevents" ${outdir}/STtW.txt > temp && mv temp ${outdir}/STtW.txt 
- printf "crosssection: %s\n" ${xcSTtW} >> ${outdir}/STtW.txt
- root -l -b -q countevents.cxx\('"STtW.txt"'\)
-fi
-
-if [ ${doWJets} = true ]
-then
- printf "Making WJets\n" 
- grep -v "crosssection" ${outdir}/WJets.txt > temp && mv temp ${outdir}/WJets.txt 
- grep -v "nrevents" ${outdir}/WJets.txt > temp && mv temp ${outdir}/WJets.txt 
- printf "crosssection: %s\n" ${xcWJets} >> ${outdir}/WJets.txt
- root -l -b -q countevents.cxx\('"WJets.txt"'\)
-fi
-
-if [ ${doZHtoLLbb} = true ]
-then
- printf "Making ZHtoLLbb\n" 
- grep -v "crosssection" ${outdir}/ZHtoLLbb.txt > temp && mv temp ${outdir}/ZHtoLLbb.txt 
- grep -v "nrevents" ${outdir}/ZHtoLLbb.txt > temp && mv temp ${outdir}/ZHtoLLbb.txt 
- printf "crosssection: %s\n" ${xcZHtoLLbb} >> ${outdir}/ZHtoLLbb.txt
- root -l -b -q countevents.cxx\('"ZHtoLLbb.txt"'\)
-fi
-
-if [ ${doWW} = true ]
-then
- printf "Making WW\n" 
- grep -v "crosssection" ${outdir}/WW.txt > temp && mv temp ${outdir}/WW.txt 
- grep -v "nrevents" ${outdir}/WW.txt > temp && mv temp ${outdir}/WW.txt 
- printf "crosssection: %s\n" ${xcWW} >> ${outdir}/WW.txt
- root -l -b -q countevents.cxx\('"WW.txt"'\)
-fi
-
-if [ ${doZZ} = true ]
-then
- printf "Making ZZ\n" 
- grep -v "crosssection" ${outdir}/ZZ.txt > temp && mv temp ${outdir}/ZZ.txt 
- grep -v "nrevents" ${outdir}/ZZ.txt > temp && mv temp ${outdir}/ZZ.txt 
- printf "crosssection: %s\n" ${xcZZ} >> ${outdir}/ZZ.txt
- root -l -b -q countevents.cxx\('"ZZ.txt"'\)
-fi
-
-if [ ${doWZ} = true ]
-then
- printf "Making WZ\n" 
- grep -v "crosssection" ${outdir}/WZ.txt > temp && mv temp ${outdir}/WZ.txt 
- grep -v "nrevents" ${outdir}/WZ.txt > temp && mv temp ${outdir}/WZ.txt 
- printf "crosssection: %s\n" ${xcWZ} >> ${outdir}/WZ.txt
- root -l -b -q countevents.cxx\('"WZ.txt"'\)
-fi
-
-if [ ${doSignal} = true ]
-then
- printf "Making Signal\n" 
- grep -v "crosssection" ${outdir}/Signal.txt > temp && mv temp ${outdir}/Signal.txt 
- grep -v "nrevents" ${outdir}/Signal.txt > temp && mv temp ${outdir}/Signal.txt 
- printf "crosssection: %s\n" ${xcSignal} >> ${outdir}/Signal.txt
- root -l -b -q countevents.cxx\('"Signal.txt",kTRUE'\)
-fi
+countmanylists ${doSignal} "Signal" ${xcSignal}
 
