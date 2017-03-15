105aa3419f66559b7e900dba6d292d919085c290
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-03-14 20:09:50 -0500

    more variables, inpath/outpath

--- analyzers/main.C
+++ analyzers/main.C
@@ -27,17 +27,27 @@ int main(int argc, char **argv){
  char *sxname = (char*)"";
  char *sevts = (char*)"-1";
  char *jettype = (char*)"ALLCALOJETS";
+ char *inpath = (char*)"../lists";
+ char *outpath = (char*)"../roots";
  int index;
  int s;
  int l;
  int x;
  int j;
+ int i;
+ int o;
 
  opterr = 0;
 
- while ((s = getopt (argc, argv, "s:l:x:e:j:")) != -1)
+ while ((s = getopt (argc, argv, "s:l:x:e:j:i:o:")) != -1)
   switch (s)
    {
+   case 'o':
+    outpath = optarg;
+    break;
+   case 'i':
+    inpath = optarg;
+    break;
    case 'j':
     jettype = optarg;
     break;
@@ -80,16 +90,21 @@ int main(int argc, char **argv){
  TString TSevts   = TString(sevts);
  Int_t TIevts     = TSevts.Atoi();
  TString Tjettype = TString(jettype);
+ TString Tinpath   = TString(inpath);  
+ TString Toutpath  = TString(outpath); 
 
- TString inpath = "../lists";
- TString outpath = "../roots";
-
+  printf("Tsample:  %s\n",Tsample.Data());
+  printf("Txname:   %s\n",Txname.Data());
+  printf("TSlumi:   %s\n",TSlumi.Data());
+  printf("TSevts:   %s\n",TSevts.Data());
+  printf("Tjettype: %s\n",Tjettype.Data());
  // MC vs Data
  Bool_t isMC=kTRUE;
  Bool_t makelog=kTRUE;
 
- TString outfilename=outpath +"/"+Tsample+"_"+Tjettype+Txname;
- TString inputListName=inpath+"/"+Tsample+".txt";
+ TString outfilename=Toutpath +"/"+Tsample+"_"+Tjettype+Txname;
+ TString inputListName=Tinpath+"/"+Tsample+".list";
+ TString inputInfoName=Tinpath+"/"+Tsample+".info";
 
  TChain *theChain = new TChain("treeR");
  theChain->Reset();
@@ -97,23 +112,19 @@ int main(int argc, char **argv){
  printf("Input List Name:  %s\n", inputListName.Data()) ; 
  printf("Output File Name: %s.root\n", outfilename.Data()  ) ; 
 
+ // lines read from file variables
+ std::string inputline = "";
+ TString Tinputline = "";
  std::vector<TString> inputline_dump;
 
- // sample-dependent input variables 
- Double_t nrevents     ;
- Double_t crosssection ;
-
- // open file_name_list.txt
+ // ---- Get list of files to run over and put in TChain
+ // open <samplename>.list
  std::ifstream inputfile;
  inputfile.open(inputListName);
  if( !inputfile.good() ) {
    std::cerr << "Cannot open the file: \"" << inputListName+"\""<<std::endl;
    abort();
  }
-
- // lines read from file variables
- std::string inputline = "";
- TString Tinputline = "";
  
  // we have the file open, start reading lines one by one
  while( std::getline(inputfile, inputline) ) {
@@ -121,21 +132,47 @@ int main(int argc, char **argv){
 
   // TChain needs a TString..
   Tinputline = inputline;
-
   //printf("Inputline: %s\n",Tinputline.Data());
 
   // read input file names
   if( Tinputline.Contains("/store/group") ){
-   std::cout << "Input File Name: "  << Tinputline <<  std::endl;
-   theChain->Add( "root://cmseos.fnal.gov/"+Tinputline );
+   //if( dolocal ){
+   // theChain->Add( "root://cmseos.fnal.gov/"+Tinputline );
+   //}
+   //else{
+    theChain->Add( "root://cmsxrootd.fnal.gov/"+Tinputline );
+   //}
    printf("Inputfile: %s\n",Tinputline.Data());
   }
+
+  inputline_dump.push_back(inputline);
+ } //while !inputfile.eof()
+ inputfile.close();
+
+ // sample-dependent input variables 
+ Double_t nrevents     ;
+ Double_t crosssection ;
+
+ // ---- Get sample information 
+ // open <samplename>.info
+ inputfile.open(inputInfoName);
+ if( !inputfile.good() ) {
+   std::cerr << "Cannot open the file: \"" << inputInfoName+"\""<<std::endl;
+   abort();
+ }
+ 
+ // we have the file open, start reading lines one by one
+ while( std::getline(inputfile, inputline) ) {
+  if( inputfile.fail() ) continue;
+
+  Tinputline = inputline;
+  //printf("Inputline: %s\n",Tinputline.Data());
 
   // read crosssection
   if( Tinputline.Contains("crosssection: ") ){  
    Tinputline.ReplaceAll("crosssection: ","");
    crosssection = Tinputline.Atof();
-   printf("\n  crosssection: %f\n",crosssection);
+   printf("  crosssection: %f\n",crosssection);
   }
 
   // read nr events
