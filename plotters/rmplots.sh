c07bd6c69c3d3feacd8fefbbab94094ec3bc7d89
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-04-29 14:55:02 +0200

    too many plots

---
+++ plotters/rmplots.sh
@@ -0,0 +1,6 @@
+
+rm ../plots/DY50_ALLCALOJETS_h_NoPair*
+rm ../plots/DY50_ALLCALOJETS_h_ZH*
+rm ../plots/DY50_ALLCALOJETS_h_DY*
+rm ../plots/DY50_ALLCALOJETS_h_Sig*
+rm ../plots/DY50_ALLCALOJETS_h_OffZ*
