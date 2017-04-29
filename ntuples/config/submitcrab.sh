a610d58f951dd8c52f4653195cbc156515517e55
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-04-29 17:11:22 +0200

    samples in list, comments, submit

--- ntuples/config/submitcrab.sh
+++ ntuples/config/submitcrab.sh
@@ -2,26 +2,48 @@
 
 domc=false
 dodata=false
-dosubmit=true
+dosubmit=false
 
+# start the timer
 START=$(date +%s);
 printf "Started at `date`\n\n"
 
+# make the directory where we'll submit from
 thesubdir="${subdir}/gitignore/${nversion}"
 mkdir -p ${thesubdir} 
 printf "Making submit configurations in\n ${thesubdir}\n\n"
 
+# get the DAS name mapping
+thedasmap="${listdir}/ntuple/dasmap.list"
+
 #lumi=40000. # /pb
 lumi=12900. # /pb
 
-#  MC samples
-samples=( \
- "ggZHSSbbbb" \
- "ZHHbb" \
+#  MC sample names
+mcsamples=( \
+ "ZHHbb_1"       \
+ "ZHHbb_2"       \
+ "ggZHHbb_1"     \
+ "ggZHHbb_2"     \
+ "ggZHHbb_3"     \
+ "ggZHSSbbbb_01" \
+ "ggZHSSbbbb_02" \
+ "ggZHSSbbbb_03" \
+ "ggZHSSbbbb_04" \
+ "ggZHSSbbbb_05" \
+ "ggZHSSbbbb_06" \
+ "ggZHSSbbbb_07" \
 )
 
-for samplename in ${samples[@]}
+# print which samples we're running over
+printf "For:\n"
+for samplename in ${mcsamples[@]}
+do
+ printf " ${samplename}\n"
+done
 
+# loop over mc samples
+for samplename in ${mcsamples[@]}
 do
  printf "\n******************************************************\n"
  if [ ${dosubmit} = true ]
@@ -34,6 +56,8 @@ do
  printf "******************************************************\n"
  printf "******************************************************\n\n"
 
+ datasetname="$(grep -P ${samplename} ${thedasmap} | sed -n -e "s@ ${samplename}    @@p")"
+
  submitname="submit_${samplename}"
  submitfile="${thesubdir}/${submitname}.py"
 
@@ -43,7 +67,7 @@ do
  INPUTFILES="'Summer16_23Sep2016V4_MC_L2Relative_AK8PFchs.txt', 'Summer16_23Sep2016V4_MC_L3Absolute_AK8PFchs.txt', 'Summer16_23Sep2016V4_MC.db'"
  NUNITS="'-1'"
  REQUESTNAME="'job_spring_ZZ'"
- DATASET=${datasetname}
+ DATASET="'${datasetname}'"
  OUTLFNBASE="'/eos/store/tmperry/${nversion}'"
 
  printf "WORKAREA      ${WORKAREA}    \n" 
@@ -63,6 +87,12 @@ do
  sed -i "s@REQUESTNAME@${REQUESTNAME}@g"   "${submitfile}" 
  sed -i "s@DATASET@${DATASET}@g"           "${submitfile}" 
  sed -i "s@OUTLFNBASE@${OUTLFNBASE}@g"     "${submitfile}" 
+
+ # submit the jobs
+ if [ ${dosubmit} = true ]
+ then
+  python ${submitfile}
+ fi
 
 done
 
