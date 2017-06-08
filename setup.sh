a1294eb3f7af7976725d011047bf144c1b39ed5b
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-06-08 07:52:44 -0500

    make plotdir

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export nversion="raindaydata"
-export aversion="sipmshape"
+export aversion="june7"
 
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null
 export basedir=`pwd`
@@ -9,6 +9,7 @@ popd > /dev/null
 
 export listdir="${basedir}/lists"
 export rootdir="${basedir}/roots"
+export plotdir="${basedir}/plots"
 export andir="${basedir}/analysis"
 
 export subdir="${basedir}/ntuples/config"
