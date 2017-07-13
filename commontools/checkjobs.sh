b7e33976a8407c257ace6cb64953d8f7c345f7d3
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-07-13 09:22:57 -0500

    update samplenames

--- commontools/checkjobs.sh
+++ commontools/checkjobs.sh
@@ -6,34 +6,47 @@
 printf "Files that are not done in condor: \n"
 
 samples=( \
- "DY50"                               \
- "DY5to50_HT100to200"                 \
- "DY5to50_HT200to400"                 \
- "DY5to50_HT400to600"                 \
- "DY5to50_HT600toInf"                 \
- "DY5to50_HT70to100"                  \
- "GJets_HT40To100"                    \
- "GJets_HT100To200"                   \
- "GJets_HT200To400"                   \
- "GJets_HT400To600"                   \
- "GJets_HT600ToInf"                   \
- "ST_s"                               \
- "STbar_t"                            \
- "ST_t"                               \
- "STbar_tW"                           \
- "ST_tW"                              \
- "TTJets"                             \
- "WW"                                 \
- "WZ"                                 \
- "ZZ"                                 \
- "ZH_HToBB_ZToLL"                     \
- "ggZH_HToBB_ZToLL"                   \
- "ggZH_HToSSTobbbb_MS40_ctauS100"     \
- "ggZH_HToSSTodddd_MS40_ctauS100"     \
- "WJetsToLNu"                         \
- "SingleElectron"                     \
- "SingleMuon"                         \
-
+ "DY10to50"                            \
+ "DY50"                                \
+ "ggZH_HToBB_ZToLL"                    \
+ "ggZH_HToSSTobbbb_MS40_ctauS0"        \
+ "ggZH_HToSSTobbbb_MS40_ctauS0p05"     \
+ "ggZH_HToSSTobbbb_MS40_ctauS1"        \
+ "ggZH_HToSSTobbbb_MS40_ctauS10"       \
+ "ggZH_HToSSTobbbb_MS40_ctauS100"      \
+ "ggZH_HToSSTobbbb_MS40_ctauS1000"     \
+ "ggZH_HToSSTobbbb_MS40_ctauS10000"    \
+ "GJets_HT40To100"                     \
+ "GJets_HT100To200"                    \
+ "GJets_HT200To400"                    \
+ "GJets_HT400To600"                    \
+ "GJets_HT600ToInf"                    \
+ "ST_s"                                \
+ "STbar_t"                             \
+ "ST_t"                                \
+ "STbar_tW"                            \
+ "ST_tW"                               \
+ "TTtoLL"                              \
+ "TTtoLfromTbar"                       \
+ "TTtoLfromT"                          \
+ "WG"                                  \
+ "WJetsToLNu"                          \
+ "WWToLNuLNu"                          \
+ "WWToLNuQQ"                           \
+ "WZToL3Nu"                            \
+ "WZTo3LNu"                            \
+ "WZToLNu2QorQQ2L"                     \
+ "ZG"                                  \
+ "ZH_HToBB_ZToLL"                      \
+ "ZZToLLNuNu"                          \
+ "ZZToLLQQ"                            \
+ "ZZToNuNuQQ"                          \
+ "ZZToLLLL"                            \
+ "SingleElectron"                      \
+ "SingleMuon"                          \
+ "DoubleEG"                            \
+ "DoubleMuon"                          \
+ "MuonEG"                              \
 )
 
 for sample in ${samples[@]}
@@ -41,6 +54,7 @@ do
  #chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/checker.sh"
  var=$(bash "${basedir}/submitters/gitignore/${aversion}/${sample}/checker.sh" | wc -l )
  lc=$( cat "${basedir}/submitters/gitignore/${aversion}/${sample}/checker.sh" | wc -l )
- echo "${lc}  ${var}"
+ printf "%40s %4s %4s \n" ${sample} ${lc}  ${var}
+ #echo "${sample} ${lc}  ${var}"
  
 done
--- commontools/haddjobs.sh
+++ commontools/haddjobs.sh
@@ -7,33 +7,47 @@ mkdir -p "${plotdir}/${aversion}"
 
  #"DY50"     \
 samples=( \
- "DY50"                               \
- "DY5to50_HT100to200"                 \
- "DY5to50_HT200to400"                 \
- "DY5to50_HT400to600"                 \
- "DY5to50_HT600toInf"                 \
- "DY5to50_HT70to100"                  \
- "GJets_HT40To100"                    \
- "GJets_HT100To200"                   \
- "GJets_HT200To400"                   \
- "GJets_HT400To600"                   \
- "GJets_HT600ToInf"                   \
- "ST_s"                               \
- "STbar_t"                            \
- "ST_t"                               \
- "STbar_tW"                           \
- "ST_tW"                              \
- "TTJets"                             \
- "WW"                                 \
- "WZ"                                 \
- "ZZ"                                 \
- "ZH_HToBB_ZToLL"                     \
- "ggZH_HToBB_ZToLL"                   \
- "ggZH_HToSSTobbbb_MS40_ctauS100"     \
- "ggZH_HToSSTodddd_MS40_ctauS100"     \
- "WJetsToLNu"                         \
- "SingleElectron"                     \
- "SingleMuon"                         \
+ "DY10to50"                            \
+ "DY50"                                \
+ "ggZH_HToBB_ZToLL"                    \
+ "ggZH_HToSSTobbbb_MS40_ctauS0"        \
+ "ggZH_HToSSTobbbb_MS40_ctauS0p05"     \
+ "ggZH_HToSSTobbbb_MS40_ctauS1"        \
+ "ggZH_HToSSTobbbb_MS40_ctauS10"       \
+ "ggZH_HToSSTobbbb_MS40_ctauS100"      \
+ "ggZH_HToSSTobbbb_MS40_ctauS1000"     \
+ "ggZH_HToSSTobbbb_MS40_ctauS10000"    \
+ "GJets_HT40To100"                     \
+ "GJets_HT100To200"                    \
+ "GJets_HT200To400"                    \
+ "GJets_HT400To600"                    \
+ "GJets_HT600ToInf"                    \
+ "ST_s"                                \
+ "STbar_t"                             \
+ "ST_t"                                \
+ "STbar_tW"                            \
+ "ST_tW"                               \
+ "TTtoLL"                              \
+ "TTtoLfromTbar"                       \
+ "TTtoLfromT"                          \
+ "WG"                                  \
+ "WJetsToLNu"                          \
+ "WWToLNuLNu"                          \
+ "WWToLNuQQ"                           \
+ "WZToL3Nu"                            \
+ "WZTo3LNu"                            \
+ "WZToLNu2QorQQ2L"                     \
+ "ZG"                                  \
+ "ZH_HToBB_ZToLL"                      \
+ "ZZToLLNuNu"                          \
+ "ZZToLLQQ"                            \
+ "ZZToNuNuQQ"                          \
+ "ZZToLLLL"                            \
+ "SingleElectron"                      \
+ "SingleMuon"                          \
+ "DoubleEG"                            \
+ "DoubleMuon"                          \
+ "MuonEG"                              \
 )
 
 for sample in ${samples[@]}
