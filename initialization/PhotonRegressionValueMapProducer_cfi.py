332fc8cd4fc9cd74b238bcd7d52076ef6109bc42
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-09-17 20:57:41 -0500

    disable autodetection of AOD

---
+++ initialization/PhotonRegressionValueMapProducer_cfi.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+photonRegressionValueMapProducer = cms.EDProducer('PhotonRegressionValueMapProducer',
+                                                  #presently the photon regressions use the fraction-ized (PF clustering) shower shapes
+                                                  useFull5x5 = cms.bool(False), 
+                                                  # The module automatically detects AOD vs miniAOD, so we configure both
+                                                  # this messes with using parent so I prefer explicit AOD/miniAOD
+                                                  isAOD            = cms.bool(False),
+                                                  #
+                                                  # AOD case
+                                                  #
+                                                  ebReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsEB"),
+                                                  eeReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsEE"),
+                                                  esReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsES"),
+                                                  src = cms.InputTag('gedPhotons'),
+                                                  #
+                                                  # miniAOD case
+                                                  #
+                                                  ebReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEBRecHits"),
+                                                  eeReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEERecHits"),
+                                                  esReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedESRecHits"),
+                                                  srcMiniAOD = cms.InputTag('slimmedPhotons',
+                                                                            processName=cms.InputTag.skipCurrentProcess()),
+                                                  )
---
+++ initialization/egmPhotonIDs_cff.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.SelectorUtils.tools.auxiliary_cff import DataFormat
+
+def LoadEgmIdSequence(process, dataFormat):
+    process.load("RecoEgamma.PhotonIdentification.egmPhotonIDs_cfi")
+    from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
+
+    # Load the producer module to build full 5x5 cluster shapes and whatever 
+    # else is needed for IDs
+    process.load("RecoEgamma.PhotonIdentification.PhotonIDValueMapProducer_cfi")
+
+    # Load the producer for MVA IDs. Make sure it is also added to the sequence!
+    # don't do that for now
+    #process.load("RecoEgamma.PhotonIdentification.PhotonMVAValueMapProducer_cfi")
+    process.load("RecoEgamma.PhotonIdentification.PhotonRegressionValueMapProducer_cfi")
+
+    # Load sequences for isolations computed with CITK for both AOD and miniAOD cases
+    if dataFormat== DataFormat.AOD:
+        process.load("RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationAOD_cff")
+        process.egmPhotonIDSequence = cms.Sequence(process.egmPhotonIsolationAODSequence 
+                                                   * process.photonIDValueMapProducer 
+                                                   * process.photonMVAValueMapProducer 
+                                                   * process.egmPhotonIDs 
+                                                   * process.photonRegressionValueMapProducer )
+    elif dataFormat== DataFormat.MiniAOD:
+        process.load("RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationMiniAOD_cff")
+        process.egmPhotonIDSequence = cms.Sequence(process.egmPhotonIsolationMiniAODSequence 
+                                                   * process.photonIDValueMapProducer 
+                                                   #* process.photonMVAValueMapProducer  sorry, no MVA today
+                                                   * process.egmPhotonIDs 
+                                                   * process.photonRegressionValueMapProducer )
+    else:
+        raise Exception('InvalidVIDDataFormat', 'The requested data format is different from AOD or MiniAOD')
