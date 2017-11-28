bb42f4c0a268d2efa88294015d27c1e81f59079c
Thomas Mastrianni Perry <tmperry@cern.ch>
2017-11-28 07:00:32 -0600

    local prioritization of jobs

--- ntuples/config/crab_template.py
+++ ntuples/config/crab_template.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     #config.JobType.inputFiles         = [INPUTFILES]
     config.JobType.sendExternalFolder = True
     config.JobType.maxMemoryMB        = MAXMEM
+    #config.JobType.priority           = 50 # default=10, first 10 jobs=20
     config.Data.inputDBS              = 'global'    
     config.Data.splitting             = SPLITTING # 'LumiBased' EventBased, FileBased, LumiBased (1 lumi ~= 300 events)
     config.Data.totalUnits            = NUNITS
