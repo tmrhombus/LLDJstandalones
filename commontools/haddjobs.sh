08f83652c2181ca3c21835e5f5c48f759aaf6bd9
Thomas Mastrianni Perry <tmperry@cern.ch>
2018-04-29 16:08:04 -0500

    condor/hadder/checker updates to keep up with analyzer

--- commontools/haddjobs.sh
+++ commontools/haddjobs.sh
@@ -7,54 +7,27 @@ mkdir -p "${plotdir}/${aversion}"
 mkdir -p "${plotdir}/${aversion}/logs"
 
 samples=( \
- "DY50"                             \
- "ST_s"                             \
- "STbar_t"                          \
- "ST_t"                             \
- "STbar_tW"                         \
- "ST_tW"                            \
  "TTtoLL"                           \
- "TTtoLfromTbar"                    \
- "TTtoLfromT"                       \
- "WG"                               \
- "WJetsToLNu"                       \
- "WWToLNuLNu"                       \
- "WWToLNuQQ"                        \
- "WZToL3Nu"                         \
- "WZTo3LNu"                         \
- "WZToLNu2QorQQ2L"                  \
- "ZG"                               \
- "ZH_HToBB_ZToLL"                   \
- "ZZToLLNuNu"                       \
- "ZZToLLQQ"                         \
- "ZZToNuNuQQ"                       \
- "ZZToLLLL"                         \
- "SingleElectron"                   \
- "SingleMuon"                       \
 )
-# "DY5to50_HT70To100"                \
-# "DY5to50_HT100To200"               \
-# "DY5to50_HT200To400"               \
-# "DY5to50_HT400To600"               \
-# "DY5to50_HT600ToInf"               \
-# "ggZH_HToBB_ZToLL"                 \
-# "ggZH_HToSSTobbbb_MS40_ctauS0"     \
-# "ggZH_HToSSTobbbb_MS40_ctauS0p05"  \
-# "ggZH_HToSSTobbbb_MS40_ctauS1"     \
-# "ggZH_HToSSTobbbb_MS40_ctauS10"    \
-# "ggZH_HToSSTobbbb_MS40_ctauS100"   \
-# "ggZH_HToSSTobbbb_MS40_ctauS1000"  \
-# "ggZH_HToSSTobbbb_MS40_ctauS10000" \
-# "GJets_HT40To100"                  \
-# "GJets_HT100To200"                 \
-# "GJets_HT200To400"                 \
-# "GJets_HT400To600"                 \
-# "GJets_HT600ToInf"                 \
 
-#for sample in ${samples[@]}
-for sample in $(ls ${basedir}/submitters/gitignore/${aversion}/)
+#for sample in $(ls ${basedir}/submitters/gitignore/${aversion}/) # works but dangerous?
+for sample in ${samples[@]}
 do
- chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit.sh"
- bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit.sh"
- #bash "/nfs_scratch/tperry/gitignore/${aversion}/${sample}/haddit.sh"
+
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_DY_histograms.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NoPair_histograms.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NoSel_histograms.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OPTtree.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OffZ_histograms.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_Sig_histograms.sh"
+ chmod +x "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_ZH_histograms.sh"
+
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_DY_histograms.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NoPair_histograms.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_NoSel_histograms.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OPTtree.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_OffZ_histograms.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_Sig_histograms.sh"
+ bash "${basedir}/submitters/gitignore/${aversion}/${sample}/haddit_ZH_histograms.sh"
+
 done
