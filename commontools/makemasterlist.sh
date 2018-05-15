8f604246773a0c6eb952ad5677cb0c0bf7bdf64b
Thomas Mastrianni Perry <tmperry@cern.ch>
2018-05-15 10:18:57 -0500

    AOD as default name

--- commontools/makemasterlist.sh
+++ commontools/makemasterlist.sh
@@ -4,15 +4,11 @@
 # define output directory where lists will end up
 outdir="${CMSSW_BASE}/src/LLDJstandalones/lists"
 
-#is this functionality no longer needed?
-filetype="" # miniAOD=""
-#filetype="AOD" # miniAOD=""
-
 # read first layer of storage directory save output to temp file
-xrdfs root://cmseos.fnal.gov ls ${depot}/${nversion}${filetype} > templayer1.out
+xrdfs root://cmseos.fnal.gov ls ${depot}/${nversion} > templayer1.out
 
 # initialize outfile as empty (overwrite if exists)
-echo "" > ${outdir}/allfiles${filetype}.masterlist
+echo "" > ${outdir}/allfiles.masterlist
 
 # read subsequent layers into temp files until arrive at root file
 for lineone in $(cat templayer1.out);
@@ -31,15 +27,15 @@ do
    for linefour in $(cat templayer4.out)
    do 
    
-   xrdfs root://cmseos.fnal.gov ls ${linefour} >> ${outdir}/allfiles${filetype}.masterlist 
+   xrdfs root://cmseos.fnal.gov ls ${linefour} >> ${outdir}/allfiles.masterlist 
    done
   done
  done
 done
 
 #remove bad entries from masterlist
-sed -i '/log/d' ${outdir}/allfiles${filetype}.masterlist
-sed -i '/failed/d' ${outdir}/allfiles${filetype}.masterlist
+sed -i '/log/d' ${outdir}/allfiles.masterlist
+sed -i '/failed/d' ${outdir}/allfiles.masterlist
 #save master list and remove junk files
 mv templayer1.out ${outdir}/dir.out
 rm templayer2.out
