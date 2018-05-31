38034576106d9fa0873019b483b631ed428eda7b
Daniel Diaz <ddiaz006@ucr.edu>
2018-05-30 21:36:09 -0500

    Removing my comments from setup.sh

--- submitters/submitjobs.sh
+++ submitters/submitjobs.sh
@@ -6,7 +6,7 @@
 # source xx/LLDJ/setup.sh for ${aversion}
 
 doSubmit=true
-lumi=20000  #20000   #20000     #35870
+lumi=8651 #20000   #20000     #35870
 nevents=-1
 maxfilesperjob=200   # 500=6h
 
@@ -125,51 +125,7 @@ maxfilesperjob=200   # 500=6h
 #  "DY5to50_HT600toInf_2" \
 
 samples=(  \
- "ggZH_HToSSTobbbb_MS40_ctauS1"       \
- "ggZH_HToSSTobbbb_MS40_ctauS10"      \
- "ggZH_HToSSTobbbb_MS40_ctauS100"     \
- "ggZH_HToSSTobbbb_MS40_ctauS1000"    \
- "ggZH_HToSSTobbbb_MS15_ctauS1"       \
- "ggZH_HToSSTobbbb_MS15_ctauS1000"    \
- "ggZH_HToSSTobbbb_MS55_ctauS1"       \
- "ggZH_HToSSTobbbb_MS55_ctauS100"     \
- "ZH_HToSSTobbbb_MS40_ctauS10"        \
- "ZH_HToSSTobbbb_MS40_ctauS100"       \
- "ZH_HToSSTobbbb_MS40_ctauS1000"      \
- "ZH_HToSSTobbbb_MS15_ctauS1"         \
- "ZH_HToSSTobbbb_MS15_ctauS10"        \
- "ZH_HToSSTobbbb_MS15_ctauS100"       \
- "ZH_HToSSTobbbb_MS15_ctauS1000"      \
- "ZH_HToSSTobbbb_MS55_ctauS10"        \
- "ZH_HToSSTobbbb_MS55_ctauS1000"      \
- "DY50"                               \
- "ST_s"                               \
- "ST_t"                               \
- "STbar_tW"                           \
- "ST_tW"                              \
- "TTtoLL"                             \
- "TTtoLfromTbar"                      \
- "TTtoLfromT"                         \
- "WG"                                 \
- "WJetsToLNu"                         \
- "WWToLNuLNu"                         \
- "WWToLNuQQ"                          \
- "WZToL3Nu"                           \
- "WZTo3LNu"                           \
- "WZToLNu2QorQQ2L"                    \
- "ZG"                                 \
- "ZZToLLNuNu"                         \
- "ZZToLLQQ"                           \
- "ZZToNuNuQQ"                         \
- "ZZToLLLL"                           \
- "GJets_HT40To100"                    \
- "GJets_HT100To200"                   \
- "GJets_HT200To400"                   \
- "GJets_HT400To600"                   \
- "DY5to50_HT100To200"                 \
- "DY5to50_HT200To400"                 \
- "DY5to50_HT400To600"                 \
- "DY5to50_HT600ToInf"                 
+  "SinglePhoton_H"   \
 )
 
 
