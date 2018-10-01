fdcb95f0bdacb144da6f9e68e8530da8b5ac21e4
Ben Kreis <benjaminkreis@gmail.com>
2018-10-01 13:35:32 -0500

    Update check_sites script
    to actually check presence on T2 and T3, thereby ignoring "original placement"

--- lists/ntuple/check_sites.py
+++ lists/ntuple/check_sites.py
@@ -7,6 +7,18 @@ for line in listfile:
 
     for value in values:
         if "/AOD" in value:
-            output = os.popen('dasgoclient -query="site dataset='+value+'"').read()
-            if "T2_" not in output and "T3_" not in output:
+            output = os.popen('dasgoclient -json -query="site dataset='+value+'"').read()
+            
+            #don't bother with json parsing
+            foundsite = False 
+            outputlines = output.splitlines()
+            for outputline in outputlines:
+                #Look for T2 or T3
+                if "T2_" in outputline or "T3_" in outputline:
+                    #Look for 100% presence to filter out original placement
+                    if '"block_completion":"100.00%"' in outputline and '"block_fraction":"100.00%"' in outputline and '"dataset_fraction":"100.00%"' in outputline:
+                        foundsite = True
+
+            if not foundsite:
                 print value
+   
