ce6526779d15733034e6dcce97647eda5cdcbd86
Thomas Mastrianni Perry <tmperry@cern.ch>
2019-08-24 11:59:46 -0500

    put uncertainties back into main.C

--- analyzers/main.C
+++ analyzers/main.C
@@ -236,22 +236,22 @@ int main(int argc, char **argv){
 
  std::vector<TString> unccategories;
  unccategories.push_back("");
-// if( isMC ){
-//   unccategories.push_back("_EGSUp");
-//   unccategories.push_back("_EGSDown");
-//   unccategories.push_back("_MESUp");
-//   unccategories.push_back("_MESDown");
-//   //unccategories.push_back("_JESUp");
-//   //unccategories.push_back("_JESDown");
-//   unccategories.push_back("_AMaxUp");
-//   unccategories.push_back("_AMaxDown");
-//   unccategories.push_back("_IPSigUp");
-//   unccategories.push_back("_IPSigDown");
-//   unccategories.push_back("_TAUp");
-//   unccategories.push_back("_TADown");
-//   unccategories.push_back("_TagVarsUp");
-//   unccategories.push_back("_TagVarsDown");
-//  }
+ if( isMC ){
+   unccategories.push_back("_EGSUp");
+   unccategories.push_back("_EGSDown");
+   unccategories.push_back("_MESUp");
+   unccategories.push_back("_MESDown");
+   //unccategories.push_back("_JESUp");
+   //unccategories.push_back("_JESDown");
+   unccategories.push_back("_AMaxUp");
+   unccategories.push_back("_AMaxDown");
+   unccategories.push_back("_IPSigUp");
+   unccategories.push_back("_IPSigDown");
+   unccategories.push_back("_TAUp");
+   unccategories.push_back("_TADown");
+   unccategories.push_back("_TagVarsUp");
+   unccategories.push_back("_TagVarsDown");
+  }
  
  // make the analyzer, init some stuff
  analyzer_loop analyzer;
